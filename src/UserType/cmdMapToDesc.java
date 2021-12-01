3a7237804f90aeaba604c50c016c39527daa1b14
rdeka <raktimdeka14@gmail.com>
2021-12-01 12:19:35 +0530

    fucked up but fixed

---
+++ src/UserType/cmdMapToDesc.java
@@ -0,0 +1,14 @@
+package UserType;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+
+interface cmdMapToDesc {
+    Map <String,Runnable> cmdMap = new HashMap <>();
+    BiFunction <String,String,String> commandToDescription = (cmd,desc) -> {
+        System.console().printf(desc); return cmd;
+    };
+
+    void FunctionMapping();
+}
